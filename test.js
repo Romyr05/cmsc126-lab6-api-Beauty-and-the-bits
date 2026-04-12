e110e3efc2e198141445de34ec519124ca5ab5e1
TheJuanderer <brentmichaelmendozaup@gmail.com>
2026-04-12 17:26:30 +0800

    added a decription in test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 let x = fetch("https://pokeapi.co/api/v2/pokemon/35")
     .then(result => {return result.json()})
-    .then(object => {console.log(object)})
+    .then(object => {console.log(object)}) //this just prints what the object has
 
 
 
