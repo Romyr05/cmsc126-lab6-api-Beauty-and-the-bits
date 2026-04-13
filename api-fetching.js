65622d6c920df2d8390d4fafa4ccd9f8b5a93769
TheJuanderer <brentmichaelmendozaup@gmail.com>
2026-04-13 20:42:20 +0800

    added more type of data to fetch, and improve readability of the api fetching code

--- api-fetching.js
+++ api-fetching.js
@@ -1,37 +1,67 @@
-let pokes = [];
+let pokesArr = [];
 
 async function fetchPokes() {
-    for (i = 1; i <= 20; i++) {
-        await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`) //get the first 20 pokemon
-            .then(result => { return result.json() })
-            .then(pokemon => {
-                fetch(pokemon.forms[0].url)
-                    .then(result => { return result.json() })
-                    .then(forms => {
-                        pokes.push({
-                            name: pokemon.forms[0].name,
-                            picture: forms.sprites.front_default
-                        })
-                    })
+    for (let i = 1; i <= 5; i++) { //we will do api calls to five pokemons
+        let pokeObj = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then(result => { return result.json() })
+        
+    
+        let formObj = await fetch(pokeObj.forms[0].url).then(result => { return result.json() })
+        
+        let abilityObj = await fetch(pokeObj.abilities[0].ability.url).then(result => {return result.json()})
+        
+        //console.log(pokeObj.types)
+        
+        //gets all the ability a pokemon might have
+        ability_entries = []
+        for (let abilities of pokeObj.abilities) {
+            let resObj = await fetch(abilities.ability.url).then(res => { return res.json() })
+            ability_entries.push({
+                name: resObj.name,
+                effect: resObj.effect_entries[2].effect,
+                shortEffect: resObj.effect_entries[2].short_effect
             })
+        }
+
+        //gets all the types a pokemon has
+        type_entries = []
+        for (let types of pokeObj.types) {
+            let resObj = await fetch(types.type.url).then(res => { return res.json() })
+            //console.log(resObj.sprites['generation-iii'].colosseum.name_icon)
+            
+            type_entries.push({
+                name: types.type.name,
+                sprite: resObj.sprites['generation-iii'].colosseum.name_icon
+            })
+            
+        }
+        
+
+        //combine all the name, types, sprites, ability into one object
+        pokesArr.push({
+            name: pokeObj.name, //name of poke
+            sprite: formObj.sprites.front_default, //a png of poke
+            abilityEntries: ability_entries, //multiple objects
+            typeEntries: type_entries
+        })
+
     }
 }
 
 async function doIt() {
     await fetchPokes();
-    console.log(pokes);
-    showPokes(pokes)
+    console.log(pokesArr[0].typeEntries);
+    showPokes(pokesArr)
 }
 
 function showPokes(pokesArr) {
+    let pokeView = '<table>'
     pokesArr.forEach(poke => {
         document.getElementById('view').innerHTML += `
         <table>
             <tr>
                 <td> ${poke.name} </td>
-                <td><img src = '${poke.picture}'> </td>
+                <td><img src = '${poke.sprite}'> </td>
             </tr>
-        
         </table>`})
 
     
--- test.js
+++ test.js
@@ -5,35 +5,95 @@ let x = fetch("https://pokeapi.co/api/v2/pokemon/35")
 */
 
 /* an pokemon object has the attributes
-pokemon.abilites[0...x] -> for abilities
-forms.sprites[0].front_default -> for pictures of pokemon
-pokemon.forms[0].name
-pokemon.forms[0].url
+pokemonObj.abilites[0...x] -> for abilities
+formsObj.sprites[0].front_default -> for pictures of pokemon
+pokemonObj.forms[0].name -> name of pokemon
+pokemonObj.forms[0].url -> for use in fetching the sprite
+abilityObj.effect_entries[2].effect -> english version of the effect of an ability
+
+pokes.push({
+            pokemon: pokeObj,
+            name: pokeObj.name,
+            sprite: formObj.sprites.front_default,
+            abilities: pokeObj.abilities
+        })
 
 */
-let pokes = [];
+let pokemon = {};
+let pokesArr = [];
+
 
 async function fetchPokes() {
-    for (i = 1; i <= 20; i++) {
-        await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`) //get the first 20 pokemon
-            .then(result => { return result.json() })
-            .then(pokemon => {
-                fetch(pokemon.forms[0].url)
-                    .then(result => { return result.json() })
-                    .then(forms => {
-                        pokes.push({
-                            name: pokemon.forms[0].name,
-                            picture: forms.sprites.front_default
-                        })
-                    })
+    let effectEntryEn;
+
+    for (let i = 1; i <= 1; i++) { //we will do api calls to five pokemons
+        let pokeObj = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then(result => { return result.json() })
+        
+    
+        let formObj = await fetch(pokeObj.forms[0].url).then(result => { return result.json() })
+        
+        let abilityObj = await fetch(pokeObj.abilities[0].ability.url).then(result => {return result.json()})
+        
+        //console.log(pokeObj.types)
+        
+        //gets all the ability a pokemon might have
+        ability_entries = []
+        for (let abilities of pokeObj.abilities) {
+            let resObj = await fetch(abilities.ability.url).then(res => { return res.json() })
+            ability_entries.push({
+                abilityName: resObj.name,
+                abilityEffect: resObj.effect_entries[2].effect,
+                abilityShortEffect: resObj.effect_entries[2].short_effect
             })
+        }
+
+        //gets all the types a pokemon has
+        type_entries = []
+        for (let types of pokeObj.types) {
+            let resObj = await fetch(types.type.url).then(res => { return res.json() })
+            console.log(resObj.sprites['generation-iii'].colosseum.name_icon)
+            
+            type_entries.push({
+                name: types.type.name,
+                sprite: resObj.sprites['generation-iii'].colosseum.name_icon
+            })
+            
+        }
+        
+
+        //combine all the name, types, sprites, ability into one object
+        pokesArr.push({
+            name: pokeObj.name, //name of poke
+            sprite: formObj.sprites.front_default, //a png of poke
+            abilityEntries: ability_entries, //multiple objects
+            typeEntries: type_entries
+        })
+
     }
+    
+    
+
+    
+    
 }
 
 async function doIt() {
     await fetchPokes();
-    await console.log(pokes);
+    console.log(pokesArr[0].typeEntries);
+    //await showPokes(pokes);
+   
 }
 
-doIt();
+async function showPokes(pokesArr) {
+    pokesArr.forEach(poke => {
+        console.log(poke.name)
+        console.log(poke.picture)
+        poke.abilities.forEach(abilityObj => {
+            let ability = abilityObj
+            console.log(ability)
+        })
+    })
+}
+
+doIt()
 
